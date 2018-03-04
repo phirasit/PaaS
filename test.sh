f6bcddbec01e734d7334b8babb352e9ed281180d
Phirasit <phirasit@localhost>
2018-03-04 11:37:25 +0700

    fix test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 for f in testcases/*.in; do
 	out=${f::-3}.out
-	dif=$(./palindrome < $f | diff - $out)
+	dif=$(./palindrome < $f | diff --strip-trailing-cr - $out)
 	echo $(cat $f):$(cat $out) | tr -d '\n'
 	if [ "$dif" == "" ]; then
 		echo " success"
